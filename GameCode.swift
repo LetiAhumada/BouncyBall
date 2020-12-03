8523cf7c9682ba429b558932c46856442e0c687c
Leticia Ahumada <lahumada@colegiobrains.com>
2020-12-03 13:07:51 +0100

    BouncyBall code. I try to send it to my iPad Pro to see if it works but a couple of errors appears. Keep working on that.

--- GameCode.swift
+++ GameCode.swift
@@ -1,5 +1,45 @@
 import Foundation
 
+let circle1 = OvalShape(width: 40, height: 40)
+//barrier
+let barrierWidth = 300.0
+let barrierHeight = 25.0
+let barrierPoints = [
+    Point(x: 0, y: 0),
+    Point(x: 0, y: barrierHeight),
+    Point(x: barrierWidth, y:
+       barrierHeight),
+    Point(x: barrierWidth, y: 0)
+]
+let barrier = PolygonShape(points:
+   barrierPoints)
+//funnel
+let funnelPoint = [
+    Point(x: 0, y: 50),
+        Point(x: 80, y: 50),
+        Point(x: 60, y: 0),
+        Point(x: 20, y: 0)
+]
+let funnel = PolygonShape(points: funnelPoint)
+//target
+let targetPoints = [
+    Point(x: 10, y: 0),
+    Point(x: 0, y: 10),
+    Point(x: 10, y: 20),
+    Point(x: 20, y: 10)
+]
+let target = PolygonShape(points: targetPoints)
+func setupTarget() {
+    target.position = Point(x: 200, y: 400)
+    target.hasPhysics = true
+    target.isImmobile = true
+    target.isImpermeable = false
+    target.fillColor = .red
+    target.name = "target"
+    
+    scene.add(target)
+}
+
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
 Use it to set up and start your app.
@@ -11,5 +51,33 @@ of a function.
 */
 
 func setup() {
-    
+    circle1.position = Point(x:250, y:400)
+    circle1.hasPhysics = true
+    circle1.fillColor = .yellow
+    scene.add(circle1)
+    //barrier code
+    barrier.position = Point(x: 200, y: 150)
+    barrier.hasPhysics = true
+    barrier.isImmobile = true
+    barrier.fillColor = .gray
+    scene.add(barrier)
+    //funnel code
+    funnel.position = Point(x: 200, y:
+    scene.height - 25)
+    funnel.fillColor = .orange
+    scene.add(funnel)
+    funnel.onTapped = dropBall
+    //target code
+    setupTarget()
+    // collisions
+
+}
+
+func dropBall() {
+    circle1.position = funnel.position
+}
+
+func ballCollided(with otherShape: Shape) {
+    if otherShape.name != "target" { return }
+    otherShape.fillColor = .red
 }
